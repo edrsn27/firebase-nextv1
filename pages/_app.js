38987ee9149c88b4dc507ad36881feefa8cb18d0
ederson <edersonvillegas50@gmail.com>
2022-05-30 21:08:05 +0800

    initial setup
    add bootstrap react

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// import App from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default App;

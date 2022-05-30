8c7b43f9eb34f95fc251c731b6cd5ebaeddf9ca9
ederson <edersonvillegas50@gmail.com>
2022-05-30 21:25:30 +0800

    add sign-up component

---
+++ components/SignUp.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Card>
+      <h2 className="text-center mb-4">Sign up</h2>
+      <Card.Body>
+        <Form onSubmit={submit}>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confrim password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="m">
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
---
+++ pages/sign-up.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SignUpComponent from "../components/SignUp";
+import { Container } from "react-bootstrap";
+export default function SignUp() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "50vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <SignUpComponent />
+      </div>
+    </Container>
+  );
+}
